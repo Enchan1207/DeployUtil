12760c3f74be9ffb0a8ecc126f90a02314c50e38
Enchan <enchant0130@gmail.com>
2020-10-27 22:08:22 +0900

    [Update] php mode detection

--- index.php
+++ index.php
@@ -99,9 +99,12 @@
 
     // レスポンスを返しつつ、エラーメッセージをログに吐き出す
     function throwErrorResponse($responseCode = -1, $message = "", $filename="error.log"){
-        // CGI版のPHPだとここで500になるらしい
-        // header('HTTP', true, $responseCode);
-        logging("$responseCode: $message");
+        // CGI版はheader関数が使えないので、php_sapi_name()で動作モードを確認
+        $phpMode = strtolower(php_sapi_name());
+        if(strpos($phpMode, "cgi") === FALSE){
+            // header('HTTP', true, $responseCode);
+        }
+        logging("mode: $phpMode  reponse: $responseCode:  message: $message");
     }
 
     // ログファイルに書き出す
